04a6a01d331d44f4963ccccc196495b5f581d140
Anthony Brunetto <abrunetto@csumb.edu>
2024-04-17 16:14:18 -0700

    Linked main activity and login page

--- app/src/main/java/com/example/tune_trade/LoginPageActivity.java
+++ app/src/main/java/com/example/tune_trade/LoginPageActivity.java
@@ -2,13 +2,27 @@ package com.example.tune_trade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.tune_trade.databinding.ActivityLoginPageBinding;
+import com.example.tune_trade.databinding.ActivityMainBinding;
 
 public class LoginPageActivity extends AppCompatActivity {
 
+    private ActivityLoginPageBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityLoginPageBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+    }
+
+    public static Intent loginPageIntentFactory(Context context) {
+        Intent intent = new Intent(context, LoginPageActivity.class);
+        return intent;
     }
 }
--- app/src/main/java/com/example/tune_trade/MainActivity.java
+++ app/src/main/java/com/example/tune_trade/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.tune_trade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import android.view.View;
+
+import com.example.tune_trade.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.goToLoginPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = LoginPageActivity.loginPageIntentFactory(getApplicationContext());
+                startActivity(intent);
+            }
+        });
+
+        binding.goToSignUpPageButton.setOnClickListener(new View.OnClickListener() {
+            // TODO: implement onClick to open sign up page
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
