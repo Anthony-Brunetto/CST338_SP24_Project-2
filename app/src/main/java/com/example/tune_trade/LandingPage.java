d7b03783b25302b9b789ce852f59ac9e1698228a
mohmd1087 <142373863+mohmd1087@users.noreply.github.com>
2024-04-17 14:24:06 -0700

    Layout for landing page

---
+++ app/src/main/java/com/example/tune_trade/LandingPage.java
@@ -0,0 +1,44 @@
+package com.example.tune_trade;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.tune_trade.databinding.ActivityLandingPageBinding;
+
+import java.util.Locale;
+
+public class LandingPage extends AppCompatActivity {
+    ActivityLandingPageBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+        binding = ActivityLandingPageBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.LogoutButtonLandingPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = MainActivity.MainActivityIntentFactory(getApplicationContext());
+//                startActivity(intent);
+            }
+        });
+
+
+
+        
+    }
+
+//    public static Intent LandingPageIntentFactory(Context context) {
+//        Intent intent = new Intent(context, MainActivity.class);
+//        return intent;
+//    }
+}
+
+
