7c926deb965867d5f2021a7f75ca5d1accdbfe44
mohmd1087 <142373863+mohmd1087@users.noreply.github.com>
2024-04-26 02:07:51 -0700

    Made the signup procedure code more efficient

--- app/src/main/java/com/example/tune_trade/SignUpPageActivity.java
+++ app/src/main/java/com/example/tune_trade/SignUpPageActivity.java
@@ -36,16 +36,11 @@ public class SignUpPageActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 signup(isAdmin);
-//                Intent intent = MainActivity.MainActivityIntentFactory(getApplicationContext());
-//                startActivity(intent);
-
-
             }
         });
     }
 
     private void signup(boolean admin) {
-        if (!admin) {
             if (usernameChecked) {
                 return;
             }
@@ -76,70 +71,36 @@ public class SignUpPageActivity extends AppCompatActivity {
                         Log.d("UserRepository", "User found: " + user.getUsername());
                         Toast.makeText(SignUpPageActivity.this, "Username is already taken, please choose a different username", Toast.LENGTH_SHORT).show();
                     } else {
-                        Log.d("UserRepository", "User not found");
-                        User user1 = new User(username, password, Address);
-                        user1.setUsername(username);
-                        user1.setPassword(password);
-                        user1.setAddress(Address);
-                        repository.insertUser(user1);
-                        usernameChecked = true;
-                        Toast.makeText(SignUpPageActivity.this, "User Added", Toast.LENGTH_SHORT).show();
-                        Intent intent = MainActivity.MainActivityIntentFactory(getApplicationContext());
-                        startActivity(intent);
-                    }
-                }
-
-            });
-        } else if (admin) {
-
-            if (usernameChecked) {
-                return;
-            }
-            String username = binding.usernameSignupTextBox.getText().toString();
-            String password = binding.passwordSignupTextBox.getText().toString();
-            String re_enter_password = binding.reEnterPasswordSignupTextBox.getText().toString();
-            String Address = binding.addressSignupTextBox.getText().toString();
-
-
-            if (username.isEmpty() || password.isEmpty() || Address.isEmpty()) {
-
-                Toast.makeText(this, "Please enter username and password", Toast.LENGTH_SHORT).show();
-                return;
-            }
-
-
-            if (!password.equals(re_enter_password)) {
-                Toast.makeText(this, "Passwords don't match", Toast.LENGTH_LONG).show();
-                return;
-            }
-            repository.getUserByUsername(username).observe(this, new Observer<User>() {
-                @Override
-                public void onChanged(User user) {
-                    if (user != null) {
-                        if (usernameChecked) {
-                            return;
+                        if(!admin){
+                            Log.d("UserRepository", "User not found");
+                            User user1 = new User(username, password, Address);
+                            user1.setUsername(username);
+                            user1.setPassword(password);
+                            user1.setAddress(Address);
+                            repository.insertUser(user1);
+                            usernameChecked = true;
+                            Toast.makeText(SignUpPageActivity.this, "User Added", Toast.LENGTH_SHORT).show();
+                            Intent intent = MainActivity.MainActivityIntentFactory(getApplicationContext());
+                            startActivity(intent);
+                        } else if (admin) {
+                            Log.d("UserRepository", "User not found");
+                            User user1 = new User(username, password, Address);
+                            user1.setUsername(username);
+                            user1.setPassword(password);
+                            user1.setAddress(Address);
+                                user1.setAdmin(true);
+                            repository.insertUser(user1);
+                            usernameChecked = true;
+                            Toast.makeText(SignUpPageActivity.this, "Admin User Added", Toast.LENGTH_SHORT).show();
+                            Intent intent = MainActivity.MainActivityIntentFactory(getApplicationContext());
+                            startActivity(intent);
                         }
-                        Log.d("UserRepository", "User found: " + user.getUsername());
-                        Toast.makeText(SignUpPageActivity.this, "Username is already taken, please choose a different username", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Log.d("UserRepository", "User not found");
-                        User user1 = new User(username, password, Address);
-                        user1.setUsername(username);
-                        user1.setPassword(password);
-                        user1.setAddress(Address);
-                        user1.setAdmin(true);
 
-                        repository.insertUser(user1);
-                        usernameChecked = true;
-                        Toast.makeText(SignUpPageActivity.this, "Admin User Added", Toast.LENGTH_SHORT).show();
-                        Intent intent = MainActivity.MainActivityIntentFactory(getApplicationContext());
-                        startActivity(intent);
                     }
                 }
 
             });
         }
-    }
 
     public static Intent SignUpPageIntentFactory(Context context, boolean isadmin){
         Intent intent = new Intent(context, SignUpPageActivity.class);
