3010226d370724da7ed73657581df8793b74d4f3
mohmd1087 <142373863+mohmd1087@users.noreply.github.com>
2024-05-01 18:00:23 -0700

    Delete in ProductDAO.java

--- app/src/main/java/com/example/tune_trade/database/ProductDAO.java
+++ app/src/main/java/com/example/tune_trade/database/ProductDAO.java
@@ -2,6 +2,7 @@ package com.example.tune_trade.database;
 
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
+import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
@@ -15,6 +16,9 @@ import java.util.List;
 public interface ProductDAO {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Product product);
+
+    @Delete
+    void Delete(Product product);
 
     @Query("SELECT * from " + TuneTradeDatabase.productTable)
     List<Product> getAllRecords();
